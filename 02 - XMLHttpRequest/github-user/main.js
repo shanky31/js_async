6856672366026734283c55aec4ce5c110b41c2e3
Gangaprasad Mohite <gemohite@gmail.com>
2019-10-10 09:44:22 +0530

    complete xhr

--- 02 - XMLHttpRequest/github-user/main.js
+++ 02 - XMLHttpRequest/github-user/main.js
@@ -1,1 +1,31 @@
 // Your code goes here
+var input = document.getElementById("input");
+var inputsubmit = document.getElementById("inputsubmit");
+var user = document.querySelector(".user");
+
+function Card(res) {
+    user.innerHTML = ` 
+    <li>
+        <div class="bio-flex">
+            <a class="githuburl" href="${res.html_url}" target="_blank"><img src=${res.avatar_url}></a>
+            <div>
+                <p class="name">${res.name}</p>
+                <a class="githuburl" href="${res.html_url}" target="_blank">Go to Github</a>
+            </div>
+        </div>
+        <div class="sub">
+            <p>Followers ${res.followers}<p>
+            <p>Following ${res.following}</p>
+            <p>Repos ${res.public_repos}</p>
+           </div>
+    </li>`
+    setTimeout(() => result(), 500)
+}
+function userapi(event){
+    var xhr = new XMLHttpRequest;
+    xhr.open("GET",`https://api.github.com/users/${input.value}`);
+    xhr.onload = () => Card(JSON.parse(xhr.response));
+    xhr.send();
+}
+
+inputsubmit.addEventListener("click",userapi)
